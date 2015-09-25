f958f930b5864d00b053616830d18fc4feca6e50
Mike Barkas <mike@barkas.com>
2015-09-25 17:22:48 -0400

    Start branch loop.

--- branches.sh
+++ branches.sh
@@ -6,4 +6,15 @@
 # Display branch description.
 # git config branch.[branch name].description
 
+branch=""
+branches=`git branch --list`
 
+while read -r branch; do
+  branch_name=${branch}
+  description=`git config branch.$branch_name.description`
+  if [ "${branch::1}" == "*" ]; then
+    printf "$branch $description \n"
+  else
+    printf "$branch $description \n"
+  fi
+done <<< "$branches"
