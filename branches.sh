e57ba7aedbdac94f8c5af2a5b80aebc9daf8046b
Mike Barkas <mike@barkas.com>
2015-11-09 21:27:59 -0500

    Changed while loop to a for in.

--- branches.sh
+++ branches.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
-#set -x
+
 # Set branch description.
 # git branch --edit-description
 
 # Display branch description.
 # git config branch.[branch name].description
 
-branches=`git branch --list`
-
-while read -r branches; do
-  branch_name=`git rev-parse --abbrev-ref HEAD`
-  description=`git config branch.$branch_name.description`
-  if [ "${branch::1}" == "*" ]; then
-    printf "${branch_name} : ${description} \n"
+BRANCHES=`git branch | cut -c 3-`
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+for BRANCH in ${BRANCHES}; do
+  DESCRIPTION=`git config branch.${BRANCH}.description`
+  if [ "${CURRENT_BRANCH}" == "${BRANCH}" ]; then
+    printf "# ${BRANCH} ${DESCRIPTION} \n"
   else
-    printf "${branch_name} : ${description} \n"
+    printf "${BRANCH} : ${DESCRIPTION} \n"
   fi
-done <<< "$branches"
+done
