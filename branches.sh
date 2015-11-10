fdf62a42170477ec116d08c54fbf462a1cc259f1
Mike Barkas <mike@barkas.com>
2015-11-09 20:24:32 -0500

    Experiment with branch name.

--- branches.sh
+++ branches.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
-
+#set -x
 # Set branch description.
 # git branch --edit-description
 
 # Display branch description.
 # git config branch.[branch name].description
 
-branch=""
 branches=`git branch --list`
 
-while read -r branch; do
-  branch_name=${branch}
+while read -r branches; do
+  branch_name=`git rev-parse --abbrev-ref HEAD`
   description=`git config branch.$branch_name.description`
   if [ "${branch::1}" == "*" ]; then
-    printf "$branch $description \n"
+    printf "${branch_name} : ${description} \n"
   else
-    printf "$branch $description \n"
+    printf "${branch_name} : ${description} \n"
   fi
 done <<< "$branches"
