d99f82def5882e070fdf62551d49703698b616c5
Mike Barkas <mike@barkas.com>
2015-12-17 23:04:20 -0500

    Set spacing to align text output.

--- branches.sh
+++ branches.sh
@@ -11,8 +11,8 @@ CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 for BRANCH in ${BRANCHES}; do
   DESCRIPTION=`git config branch.${BRANCH}.description`
   if [ "${CURRENT_BRANCH}" == "${BRANCH}" ]; then
-    printf "# ${BRANCH} ${DESCRIPTION} \n"
+    printf "* ${BRANCH} : ${DESCRIPTION} \n"
   else
-    printf "${BRANCH} : ${DESCRIPTION} \n"
+    printf "  ${BRANCH} : ${DESCRIPTION} \n"
   fi
 done
